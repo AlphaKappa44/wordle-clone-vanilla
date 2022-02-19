2ed930d7a2b745755a83d7dae0d94c079ef3ab6c
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2022-02-19 11:01:15 +0100

    setAttribute and a handleClick to keybord keys

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// create the tiles
-const tileDisplay = document.querySelector('.tile-container');
-
 // create keyboard
 // select keys
 const keyboard = document.querySelector('.key-container');
@@ -10,10 +7,20 @@ const keys = ['A', 'Z', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
 'Q', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'ENTER',
 'W', 'X', 'C', 'V', 'B', 'N'];
 
+const handleClick = (event) => {
+    event.preventDefault();
+    console.log('click', event.target);
+}
+
 // assign letters to keys
 keys.forEach(key => {
     const buttonElement = document.createElement('button')
     buttonElement.textContent = key;
+    buttonElement.setAttribute('id', key);
+    buttonElement.addEventListener('click', handleClick);
     keyboard.append(buttonElement);
 })
+
+// create the tiles
+const tileDisplay = document.querySelector('.tile-container');
 
